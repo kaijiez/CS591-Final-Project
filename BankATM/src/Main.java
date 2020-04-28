3d1c5081f86e511fcc1a37c720ac02581cb4bdc7
Kaijie Zhou <kaijiez@bu.edu>
2020-04-27 22:39:00 -0400

    finish db structure and basic sqlite operations implementation

--- BankATM/src/Main.java
+++ BankATM/src/Main.java
@@ -1,9 +1,23 @@
+import database.SQLite;
 
 public class Main {
+	
+	private static void dbTestCases(){
+//		SQLite.init();
+		
+//		SQLite.insert("Customer", new String[]{"Name"}, new String[]{"Bob4"}, new String[]{"TEXT"});
+//		SQLite.insert("Accounts", new String[]{"Type","Amount","Customer_id"}, new String[]{"Checking","10","1"}, new String[]{"text","real","integer"});
+
+//		SQLite.update("Customer", "Name = 'Bob'", new String[]{"Name"}, new String[]{"Bobby"}, new String[]{"Text"});
+		
+//		String testQuery = "SELECT id, Name FROM Customer WHERE Name = 'Bob2' ";
+//		SQLite.query(testQuery, new String[]{"id","Name"}, new String[]{"Integer","Text"});
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		dbTestCases();
 	}
 
 }
---
+++ BankATM/src/database/SQLite.java
@@ -0,0 +1,307 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLite {
+	
+	private static Connection conn = null;
+	
+	public static void init(){
+		SQLite.createNewDatabase("Bank.db");
+		
+		SQLite.createNewTable("Customers", new String[]{"id","Name"}, 
+										   new String[]{"INTEGER","TEXT"}, 
+										   new String[]{"PRIMARY KEY","NOT NULL"},
+										   new String[]{});
+		SQLite.createNewTable("Accounts", new String[]{"id", "Type","Amount","Customer_id"}, 
+										  new String[]{"integer","text","real","integer"}, 
+										  new String[]{"primary key","not null","none","not null"},
+										  new String[]{"FOREIGN KEY (Customer_id) REFERENCES Customers(id)"});
+		SQLite.createNewTable("Loans", new String[]{"id","Amount","Customer_id","Collateral","Interest","ApplyDate","ApproveDate"}, 
+									   new String[]{"integer","real","integer","text","real","text","text"},
+									   new String[]{"primary key","none","not null","none","none","none","none"},
+									   new String[]{"FOREIGN KEY (Customer_id) REFERENCES Customers(id)"});
+		SQLite.createNewTable("Transactions", new String[]{"id", "Customer_id","Account_id","Amount","Date"},
+											  new String[]{"integer","integer","integer","real","text"},
+											  new String[]{"primary key","not null","not null","not null","none"},
+											  new String[]{"FOREIGN KEY (Customer_id) REFERENCES Customers(id),\n"+
+											  			   "FOREIGN KEY (Account_id) REFERENCES Accounts(id)"});
+		SQLite.createNewTable("HeldStocks", new String[]{"id","PriceBoughtAt","StockMarket_id","Customer_id","DateBought","Amount"}, 
+										    new String[]{"integer","real","integer","integer","text","integer"},
+										    new String[]{"primary key","not null","not null","not null","none","not null"},
+										    new String[]{"FOREIGN KEY (StockMarket_id) REFERENCES StockMarket(id),\n"+
+										    			 "FOREIGN KEY (Customer_id) REFERENCES Customers(id)"});
+		SQLite.createNewTable("StockMarket", new String[]{"id","Name","Price"}, 
+											 new String[]{"integer","text","real"},
+											 new String[]{"primary key","not null","not null"},
+											 new String[]{});
+	}
+	
+	private static Connection connect(String dbName) {
+        // SQLite connection string
+		String url = "jdbc:sqlite:./src/database/" + dbName;
+        
+        try {
+        	if(conn==null){
+        		conn = DriverManager.getConnection(url);
+        	}
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+	
+	public static void createNewDatabase(String dbName) {
+
+        
+
+        try (
+        	Connection conn = connect(dbName)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+	
+	
+	public static void createNewTable(String tableName, String[] columnNames, String[] columnTypes, String[] columnConstraints , String[] foreignKeys) {
+        //make sure all three string[] are of same length
+		
+        // SQL statement for creating a new table
+		String columns="";
+		try{
+			for(int i=0;i<columnNames.length;i++){
+				//handle comma issue in last line
+				if(i!=columnNames.length-1){
+					//check if there is constraint for this column, "none" indicates no constraints
+					if(columnConstraints[i].toLowerCase().equals("none")){
+						columns+=" "+columnNames[i]+" "+columnTypes[i].toUpperCase()+",\n";
+					}
+					else{
+						columns+=" "+columnNames[i]+" "+columnTypes[i].toUpperCase()+" "+columnConstraints[i].toUpperCase()+",\n";
+					}
+				}
+				else{
+					
+					if(columnConstraints[i].toLowerCase().equals("none")){
+						if(foreignKeys.length<=0){
+							columns+=" "+columnNames[i]+" "+columnTypes[i].toUpperCase()+"\n";
+						}
+						else{
+							columns+=" "+columnNames[i]+" "+columnTypes[i].toUpperCase()+",\n";
+						}
+						
+					}else{
+						if(foreignKeys.length<=0){
+							columns+=" "+columnNames[i]+" "+columnTypes[i].toUpperCase()+" "+columnConstraints[i].toUpperCase()+"\n";
+						}
+						else{
+							columns+=" "+columnNames[i]+" "+columnTypes[i].toUpperCase()+" "+columnConstraints[i].toUpperCase()+",\n";
+						}
+					}
+				}
+			}
+			
+		}catch(Exception e){
+			System.out.println("all input arrays need to have the same length ");
+		}
+		
+		String foreign=" ";
+		for(String s:foreignKeys){
+			foreign+=s+",\n";
+		}
+		//remove extra comma in last line
+		if(foreignKeys.length>0){
+			foreign = foreign.substring(0,foreign.length()-2)+"\n";
+			foreign+="	ON UPDATE CASCADE\n	ON DELETE CASCADE\n";
+		}
+		
+        String sql = "CREATE TABLE IF NOT EXISTS "+tableName +" (\n"
+                + columns
+                +foreign
+                + ");";
+        
+        System.out.println(sql);
+        try (
+        	Connection conn = connect("Bank.db");
+            Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+	
+	// id refers to the unique id for the record in the table
+	public static void delete(String tableName, int id) {
+        String sql = "DELETE FROM "+tableName+" WHERE id = ?";
+
+        try (Connection conn = connect("Bank.db");
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // set the corresponding param
+            pstmt.setInt(1, id);
+            // execute the delete statement
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+	
+	// example insert statement: "INSERT INTO test(name,capacity) VALUES(?,?)"
+	//columns:	[col1, col2, col3]
+	//values:	ex:["value","2","3.3"]
+	//types:	ex:[text,real,integer]
+	//no validation for foreign key
+	public static void insert(String tableName, String[] columns, String[] values, String[] types) {
+		
+        String columnString="";
+        String valueString="";
+        try{
+        	for(int i=0; i<columns.length;i++){
+            	if(i==0){
+            		columnString+="("+columns[i];
+            		valueString+=" VALUES(?";
+            	}
+            	else if(i==columns.length-1){
+            		columnString+=","+columns[i]+")";
+            		valueString+=",?)";
+            	}
+            	else{
+            		columnString+=","+columns[i];
+            		valueString+=",?";
+            	}
+            	
+            }
+        	if(columns.length<2){
+        		columnString+=")";
+        		valueString+=")";
+        	}
+        }catch(Exception e){
+        	System.out.println("all input arrays need to have the same length");
+        }
+
+        String sql= "INSERT INTO "+tableName+columnString+valueString;
+        System.out.println(sql);
+
+        // set the corresponding param
+        try (
+    		Connection conn = connect("Bank.db");
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        	for(int i=0; i<values.length;i++){
+        		if(types[i].toLowerCase().equals("text")){
+        			pstmt.setString(i+1, values[i]);
+        		
+        		}
+        		else if(types[i].toLowerCase().equals("real")){
+        			pstmt.setDouble(i+1, Double.valueOf(values[i])); 
+        		}
+        		else if(types[i].toLowerCase().equals("integer")){
+        			pstmt.setInt(i+1, Integer.parseInt(values[i]));
+        		}
+        	}     
+            pstmt.executeUpdate();
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+	
+    public static void query(String query, String[] columns, String[] columnTypes){
+//        String sql = "SELECT id, name, capacity FROM test";
+        
+        try (Connection conn = connect("Bank.db");
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(query)){
+            
+        	String res="";
+            // loop through the result set
+            while (rs.next()) {
+
+                
+                for(int i=0; i<columns.length;i++){
+            		if(columnTypes[i].toLowerCase().equals("text")){
+            			res+=rs.getString(columns[i]) + "\t";
+            		
+            		}
+            		else if(columnTypes[i].toLowerCase().equals("real")){
+            			res+=rs.getDouble(columns[i]) + "\t"; 
+            		}
+            		else if(columnTypes[i].toLowerCase().equals("integer")){
+            			res+=rs.getInt(columns[i])+"\t";
+            		}
+            	}     
+                res+="\n";
+            }
+            System.out.println(res);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+// simple update query in this format
+//    UPDATE table
+//    SET column_1 = new_value_1,
+//        column_2 = new_value_2
+//    WHERE
+//        search_condition
+    
+//where:	ex: Col1 = Val1 AND Col2 = Val2 AND Col3 = Val3
+    public static void update(String tableName, String where, String[] columns, String[] values, String[] types) {
+        
+        String columnString="";
+        
+        
+        try{
+        	for(int i=0; i<columns.length;i++){
+            	if(i==columns.length-1){
+            		columnString+=columns[i]+" = ? ";
+            	}
+            	else{
+            		columnString+=columns[i]+" = ?, ";
+            	}
+            }
+
+        }catch(Exception e){
+        	System.out.println("all input arrays need to have the same length");
+        }
+        
+        String sql="UPDATE "+tableName+" SET "+columnString+"WHERE "+where;
+
+        try (Connection conn = connect("Bank.db");
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // set the corresponding param
+        	for(int i=0; i<values.length;i++){
+        		if(types[i].toLowerCase().equals("text")){
+        			pstmt.setString(i+1, values[i]);
+        		
+        		}
+        		else if(types[i].toLowerCase().equals("real")){
+        			pstmt.setDouble(i+1, Double.valueOf(values[i])); 
+        		}
+        		else if(types[i].toLowerCase().equals("integer")){
+        			pstmt.setInt(i+1, Integer.parseInt(values[i]));
+        		}
+        	}     
+            // update 
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
