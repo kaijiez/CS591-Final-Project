50277d1dde9043685b94fcefd79c8fb7b5f33ec3
Cathy Kim <cathykim@bu.edu>
2020-05-05 12:49:41 -0400

    update USD

--- BankATM/src/USD.java
+++ BankATM/src/USD.java
@@ -3,6 +3,7 @@ public class USD extends Currency{
     public USD(double amount){
         super(amount);
         this.currency = "USD";
+        this.exchange_rate = 1;
         Currency.add_currency(this);
     }
 }
